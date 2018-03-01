1b80b5f23f3f60c682ab9b0ab23d3cb60173674d
Adam Passey <adamjpassey@gmail.com>
2018-02-28 22:09:02 -0800

    Z Depth

---
+++ Assets/Scripts/PlayerDepth.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDepth : MonoBehaviour {
+
+    private SpriteRenderer renderer;
+
+	// Use this for initialization
+	void Start () {
+        renderer = GetComponentInChildren<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        renderer.sortingOrder = -Mathf.RoundToInt(transform.position.y * 1000);
+	}
+}
---
+++ Assets/Scripts/StaticDepth.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StaticDepth : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<SpriteRenderer>().sortingOrder = -Mathf.RoundToInt(transform.position.y * 1000);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
