c438c5755b4d333ee8109f129a0966226a81cf49
Adam Passey <adamjpassey@gmail.com>
2018-03-01 11:28:50 -0800

    Arrow working, adding sprites

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	public GameObject Target;
+	public GameObject ArrowPrefab;
+	public bool HasWhistle = false;
+
+	private GameObject arrow;
+
+	// Use this for initialization
+	void Start () {
+		if (HasWhistle) {
+			arrow = Instantiate (ArrowPrefab);
+			arrow.transform.position = transform.position;
+		}
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		if (HasWhistle) {
+			Vector3 rotDir = (transform.position - Target.transform.position).normalized;
+			Quaternion lookRotation = Quaternion.LookRotation(rotDir, Vector3.forward);
+			lookRotation.x = 0f;
+			lookRotation.y = 0f;
+			arrow.transform.rotation = lookRotation;
+
+			//arrow.transform.Translate (arrow.transform.forward * 2);
+			arrow.transform.position = Vector3.MoveTowards(transform.position, Target.transform.position, 0.3f);
+
+		}
+	}
+}
