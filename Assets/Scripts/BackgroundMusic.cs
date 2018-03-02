7c46aafc20f6a508e0a615a33aa98b4491d8d1d5
Adam Passey <adamjpassey@gmail.com>
2018-03-02 10:01:07 -0800

    Background music

---
+++ Assets/Scripts/BackgroundMusic.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundMusic : MonoBehaviour {
+
+	public static BackgroundMusic Instance;
+
+	// Use this for initialization
+	void Start () {
+		if (Instance != null) {
+			Destroy (gameObject);
+			return;
+		}
+		Instance = this;
+		DontDestroyOnLoad (gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
